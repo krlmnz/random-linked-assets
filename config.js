0b138b5ed5a0ee0567f964497a158bd363a1d517
Karol Munoz <karol@karolmunoz.com>
2024-10-18 00:32:11 -0400

    andean icons

---
+++ config.js
@@ -0,0 +1,42 @@
+const themes = {
+    'andean-road': {
+        'land': '#f9f5f0',
+        'water': '#D3E0E4',
+        'building': '#C8A182',
+        'park': '#B3C882',
+        'road-simple': '#C1C5C1',
+        'hillshade': '#BBBBBB'
+    },
+    'pomaire': {
+        'land': '#BD8463',
+        'water': '#535C54',
+        'building': '#944C26',
+        'park': '#B78E58',
+        'road-simple': '#C8AFA0',
+        'hillshade': '#CCCCCC'
+    },
+    'Flaminco': {
+        'land': '#FEF1EC',
+        'water': '#E4E2DB',
+        'building': '#FEF1EC',
+        'park': '#FEF1EC',
+        'road-simple': '#F9C4B0',
+        'hillshade': '#DDDDDD'
+    },
+    'Mono': {
+        'land': '#F3F3F3',
+        'water': '#DADCDB',
+        'building': '#F3F3F3',
+        'park': '#F3F3F3',
+        'road-simple': '#DADCDB',
+        'hillshade': '#EFEFEF'
+    },
+    'Drive': {
+        'land': '#FEFDFC',
+        'water': '#9FD4DA',
+        'building': '#F3F3F3',
+        'park': '#CCE5A6',
+        'road-simple': '#E8E8D3',
+        'hillshade': '#AFAFAF'
+    }
+};
